9a44c90bdb81ee7b0de8b087400eb6c5200f4a19
tais defante <taisadefante@hotmail.com>
2024-04-28 10:09:09 -0300

    primeiro commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import "../ui/styles/globals.css";
+import type { AppProps } from "next/app";
+import styles from "../ui/styles/Home.module.css";
+import Link from "next/link";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <header>
+        <Link href={"/"}>
+          <a>
+            <h1 className={styles["title"]}>
+              Meu
+              <span className={styles["title-second-word"]}>Blog</span>
+            </h1>
+          </a>
+        </Link>
+      </header>
+      <Component {...pageProps} />
+    </>
+  );
+}
+export default MyApp;
